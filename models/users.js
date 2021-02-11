9f7a08672577d6f454330ce605099d02ad3859fd
Jennifer <jennifer.nordell@outlook.com>
2021-02-11 06:07:44 -0600

    added models

---
+++ models/users.js
@@ -0,0 +1,44 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmail: true,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, { sequelize });
+
+  User.associate = (models) => {
+    User.hasMany(models.Course, {
+      as: 'courses',
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false,
+      },
+    });
+  };
+
+  return User;
+};
